1efe9aa9315f63fdfbec9a7c3979c67fe8b004fd
Clément Pit--Claudel <clement.pitclaudel@live.com>
2015-07-03 23:42:42 -0700

    Fix rsync script to mark elc files as perishable
    Use case: a package is updated in the up-to-date emacs side, and the
    previous version is removed. When syncing these changes, excluded elc
    files won't be removed, and thus the directory won't be removed either.
    Marking elc as perishable fixes the problem.

--- init/compatibility.el
+++ init/compatibility.el
@@ -1,15 +1,17 @@
 (provide 'compatibility)
 
 (defmacro when-os (os &rest body)
-  (declare (indent defun))
+  (declare (indent defun)
+           (debug t))
   `(when (if (listp ,os) (memq system-type ,os) (eq system-type ,os))
      ,@body))
 
 (when (< emacs-major-version 25)
   (when-os 'gnu/linux
-    (let ((sync-script (expand-file-name (format "rsync-packages.sh %d" emacs-major-version)))
-          (user-dir    (expand-file-name (format "elpa-%d" emacs-major-version))))
-      (shell-command sync-script)
+    (let ((sync-script (expand-file-name (format "rsync-packages.sh %d" emacs-major-version) user-emacs-directory))
+          (user-dir    (expand-file-name (format "elpa-%d" emacs-major-version) user-emacs-directory)))
+      (unless (eq (shell-command (print sync-script)) 0)
+        (error "Syncing packages (%s) failed" sync-script))
       (setq package-user-dir user-dir)))
   (when (string-equal emacs-version "24.3.1")
     (load-init-file "compat244.el"))
--- rsync-packages.sh
+++ rsync-packages.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-rsync -a --delete ~/.emacs.d/elpa/ ~/.emacs.d/"elpa-$1/" --exclude='*.elc' --exclude='__pycache__/'
+rsync -a --delete ~/.emacs.d/elpa/ ~/.emacs.d/"elpa-$1/" --filter='-p *.elc' --filter='-p __pycache__/'
