ac0ada0dda92e500aedf5210d1d4236f29ff7f21
Clément <t-clpi@microsoft.com>
2015-08-24 14:25:29 -0700

    Use a monospace font for tooltips

--- init/custom.el
+++ init/custom.el
@@ -1,10 +1,20 @@
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+    ("4e803b7d84cfcb0a124f74b77a198982d7b11705220da1a757dc013d9ceaff06" "926d2c3fd1aff97ce6b43e69072cb3de426393a76790c771995ec162b194c2c6" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(frame-background-mode (quote dark))
  '(package-selected-packages
    (quote
-    (smart-mode-line avy json-mode jinja2-mode aggressive-indent emr flycheck-pos-tip highlight-symbol haskell-mode company ag markdown-mode company-math csharp-mode expand-region ido-ubiquitous julia-mode magit multiple-cursors yasnippet flycheck-package omnisharp wgrep-ag windresize wgrep diminish latex-extra ws-butler visual-regexp tuareg smex racket-mode rainbow-delimiters popup json-rpc epc elpy company-auctex))))
+    (js2-mode smart-mode-line avy json-mode jinja2-mode aggressive-indent emr flycheck-pos-tip highlight-symbol haskell-mode company ag markdown-mode company-math csharp-mode expand-region ido-ubiquitous julia-mode magit multiple-cursors yasnippet flycheck-package omnisharp wgrep-ag windresize wgrep diminish latex-extra ws-butler visual-regexp tuareg smex racket-mode rainbow-delimiters popup json-rpc epc elpy company-auctex)))
+ '(tooltip-delay 0.3))
 
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tooltip ((t (:background "systeminfowindow" :foreground "systeminfotext" :height 0.8)))))
