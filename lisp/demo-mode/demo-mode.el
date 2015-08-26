42cdd7c6d87d803d603492a163452844d5406589
Clément <t-clpi@microsoft.com>
2015-08-25 22:15:50 -0700

    Misc improvements to demo-mode

--- lisp/demo-mode/demo-mode.el
+++ lisp/demo-mode/demo-mode.el
@@ -9,11 +9,45 @@
 
 ;;; Code:
 
+(require 'dash)
+(require 'files)
+
+(defvar demo-mode--large-font-size 200)
+(defvar demo-mode--original-font-size nil)
+
+;; (defvar-local demo-mode--overlay nil)
+
+(defun demo-mode--recenter (buf)
+  (-when-let (wnd (get-buffer-window buf))
+    (with-selected-window wnd
+      (goto-char (point-min))
+      (forward-line)
+      (recenter 0))))
+
 (define-minor-mode demo-mode
   "Sets a number of display variable to make things look good on a projector."
   :lighter nil
   (load-theme 'tangomod t)
-  (set-face-attribute 'default nil :height 200))
+  (if demo-mode
+      (progn
+        ;; Save and enlarge font
+        (unless demo-mode--original-font-size
+          (setq demo-mode--original-font-size
+                (face-attribute 'default :height)))
+        (set-face-attribute 'default nil :height demo-mode--large-font-size)
+        ;; Add an empty line at the end of the file
+        (goto-char (point-max))
+        (unless (eq (point) (point-at-bol))
+          (insert "\n"))
+        ;; Restrict to region
+        (goto-char (point-min))
+        (when (set-auto-mode-1)
+          (forward-line)
+          (narrow-to-region (point-at-bol) (point-max))))
+    (widen)
+    ;; Reset the font
+    (when demo-mode--original-font-size
+      (set-face-attribute 'default nil :height demo-mode--original-font-size))))
 
 (provide 'demo-mode)
 ;;; demo-mode.el ends here
